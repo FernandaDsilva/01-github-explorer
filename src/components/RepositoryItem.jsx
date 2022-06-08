cc9f96a43bd9ff87db8d9352cb1831e5d192a10b
fernanda dias silva <f.dias.da.silva@gmail.com>
2022-06-07 21:03:09 -0300

    :tada: Chapter I - Fundamentos do React

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,12 @@
+export function RepositoryItem(props) {
+    return (
+        <li>
+        <strong> {props.repository.name.toUpperCase()} </strong>
+        <p>{props.repository.description}</p>
+
+        <a href={props.repository.html_url}>
+            Acess Repositories
+        </a>
+    </li>
+    )
+}
